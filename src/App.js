fa8dfcc6706697fc7676f99e034f0d8cd3f88ccc
rquit <59127276+rquit@users.noreply.github.com>
2021-03-31 18:47:43 -0400

    made the css and rest of the site :)

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ const renderer = new marked.Renderer();
 function App() {
   return (
     <div className="App">
+      <Header />
       <ReactFCCtest />
       <TextBoxes />
     </div>
   );
+}
+
+const Header = () => {
+  return (
+    <div class="header-container">
+      <h1>Simple Markdown Previewer</h1>
+    </div>
+  )
 }
 
 class TextBoxes extends React.Component {
@@ -29,7 +38,7 @@ class TextBoxes extends React.Component {
 
   render () {
     return (
-    <div>
+    <div class="md-container">
       <Editor markdown={this.state.markdown} onChange={this.handleChange} />
       <Previewer markdown={this.state.markdown} />
     </div>
@@ -39,16 +48,45 @@ class TextBoxes extends React.Component {
 
 const Editor = (props) => {
   return (
-    <textarea id="editor" onChange={props.onChange} type="text" value={props.markdown} />
+    <div>
+      <h2 class="label">Input</h2>
+      <textarea id="editor" class="editor" onChange={props.onChange} type="text" value={props.markdown} />
+    </div>
   )
 }
 
 const Previewer = (props) => {
   return (
-    <div id="preview" dangerouslySetInnerHTML={{ __html: marked(props.markdown, { renderer: renderer })}} />
+    <div>
+      <h2 class="label">Output</h2>
+      <div id="preview" class="preview" dangerouslySetInnerHTML={{ __html: marked(props.markdown, { renderer: renderer })}} />
+    </div>
   )
 }
 
-const placeholder = `# This page helps you preview markdown text.`;
+const placeholder = 
+`This page helps you preview markdown text.
+
+ # This is an H1 Element.
+
+ ## This is an H2 Element.
+
+ [This is a Link.](www.google.com)
+
+ \`\`\`This is a code block!\`\`\`
+ 
+ > This is a quote
+
+ - This is a list
+   - This is a nested list
+
+ **This is bolded text.**
+
+ ![This is an image.](https://cdn.discordapp.com/avatars/132227983233515520/6ec6c62fccd13780a0a65da5d5b62ef8.png)
+
+ \`\`\`
+ And finally, this is a codeblock. 
+ \`\`\`
+`;
 
 export default App;
