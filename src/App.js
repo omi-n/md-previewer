c6103a1a0ca53cc25f25dec594951e24569b5240
rquit <59127276+rquit@users.noreply.github.com>
2021-03-31 17:25:48 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import ReactFCCtest from 'react-fcctest';
+import React from 'react';
+import marked from 'marked';
+const renderer = new marked.Renderer();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactFCCtest />
+      <TextBoxes />
     </div>
   );
 }
+
+class TextBoxes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(text) {
+    this.setState({
+      markdown: text.target.value,
+    });
+  }
+
+  render () {
+    return (
+    <div>
+      <Editor markdown={this.state.markdown} onChange={this.handleChange} />
+      <Previewer markdown={this.state.markdown} />
+    </div>
+    )
+  }
+}
+
+const Editor = (props) => {
+  return (
+    <textarea id="editor" onChange={props.onChange} type="text" value={props.markdown} />
+  )
+}
+
+const Previewer = (props) => {
+  return (
+    <div id="preview" dangerouslySetInnerHTML={{ __html: marked(props.markdown, { renderer: renderer })}} />
+  )
+}
+
+const placeholder = `# This page helps you preview markdown text.`;
 
 export default App;
